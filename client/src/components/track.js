d41fa167a1bf83674c12e79ce1e89a101c04ef96
Nathan Bogan <bogannathan@gmail.com>
2017-10-12 13:40:30 -0400

    updated track

--- client/src/components/track.js
+++ client/src/components/track.js
@@ -1,4 +1,4 @@
-mport React, {Component} from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import {Jumbotron, Button} from 'react-bootstrap';

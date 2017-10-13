7cab786a4b298df2732fef4ba16d36d0a76b0efd
Brett Newlund <bmnewlund@gmail.com>
2017-10-13 15:37:15 -0400

    Added notes app.js

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -3,13 +3,18 @@ import NavBarHeader from './nav';
 import Artist from './artists';
 import Albums from './albumid';
 
+{/* This file allows for the viewing of diffrent components and when they will be shown on the page.
+In this case <NabBarHeader> will always been seen on the page, <Albums> will be seen when the page
+is orginally loaded, and then this.props.children will enable <Albums> to be switched out with <Artists> */}
 
 
   export default class App extends Component {
       render() {
           return (
-            <div>
+            <div> 
               <NavBarHeader />
+            }
+            }
              
             {this.props.children || <Albums />} 
 

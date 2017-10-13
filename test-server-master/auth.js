e9e183388fd0dc02cfef5ac703223ed2c567a278
Madyson Ward <MadysonWard94@gmail.com>
2017-10-13 09:49:50 -0400

    MW - Added in notes/explanations on the server side

--- test-server-master/auth.js
+++ test-server-master/auth.js
@@ -1,8 +1,10 @@
-let express = require('express')
-let request = require('request')
-let querystring  = require('querystring')
-let cookieParser = require('cookie-parser')
-let router = require('express').Router()
+//SET UP
+let express = require('express');
+let request = require('request');
+let querystring  = require('querystring'); //Allows server to parse & stringify url query strings
+let cookieParser = require('cookie-parser'); //Parses cookie headers
+let router = require('express').Router(); //express router recieves requests
+
 
 router.get('/', function(req, res) {
   const config = require("./config.js")
@@ -11,14 +13,14 @@ router.get('/', function(req, res) {
   const ROOT_URL = "http://localhost:8080"
 
   let redirect_uri = 'http://localhost:8080/callback'
-  let authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
+  let authOptions = {      //authOptions contains Spotify's required body params for authorization requests
+    url: 'https://accounts.spotify.com/api/token',    
+    form: {                                       
       code: config,
       redirect_uri: redirect_uri,
       grant_type: 'client_credentials'
     },
-    headers: {
+    headers: {  //Spotify's required header param for authorization requests
       'Authorization': 'Basic ' + (new Buffer(config.client_id + ':' + config.client_secret).toString('base64'))
     },
     json: true
--- test-server-master/index.js
+++ test-server-master/index.js
@@ -1,12 +1,15 @@
-var express = require("express");
-var http = require("http");
-var bodyParser = require("body-parser");
-var app = express();
+//SET UP
+var express = require("express"); //Create express application
+var http = require("http"); //
+var bodyParser = require("body-parser"); //Allows server to find data & bring back as json objects
+var app = express(); //app object = express function
 
-app.use(require('./middleware/headers'))
+//MOUNT MIDDLEWARE
+app.use(require('./middleware/headers'));
 app.use(bodyParser.json());
-app.use('/', require('./auth'))
+app.use('/', require('./auth'));
 
+//BIND PORT/LISTEN FOR CONNECTIONS
 app.listen(3000, function(){
 	console.log("app is listening on port 3000");
 });

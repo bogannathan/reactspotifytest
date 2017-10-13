517f3d2677fc592fb4f855e8b45e288f0e3002f9
Madyson Ward <MadysonWard94@gmail.com>
2017-10-13 12:38:41 -0400

    MW - Neatened up code

--- client/src/components/splash.js
+++ client/src/components/splash.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-
 import {Jumbotron, Button} from 'react-bootstrap';
 import axios from 'axios';
 

4e86ba489de045581bf486bbee01a61e8142d671
Madyson Ward <MadysonWard94@gmail.com>
2017-10-13 15:42:06 -0400

    MW - Added notes to nav.js

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,7 +1,8 @@
+//SET UP
 import React, {Component} from 'react';
 import {Navbar, Nav, FormGroup, FormControl, NavItem, NavDropdown, DropdownButton, MenuItem, CollapsibleNav, collapseOnSelect, Header, Toggle, Collapse, Button, ButtonToolbar} from 'react-bootstrap';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
+import { connect } from 'react-redux'; //connect allows components to be connected to the redux store
+import { Link } from 'react-router'; //Link allows for declarative nagivation of an app
 import Albums from "./albumid";
 
 
@@ -22,4 +23,5 @@ class NavBarHeader extends Component {
 	}
 }
 
+//EXPORT COMPONENT
 export default NavBarHeader;
